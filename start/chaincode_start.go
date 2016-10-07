9abb6dc05bdc2c0a235ae6fdcdcfbe586e706a52
Stefano Cinardi <stefano.cinardi@gmail.com>
2016-10-07 10:27:16 +0200

    utilizzo marshaller per conversione struct in json

--- start/chaincode_start.go
+++ start/chaincode_start.go
@@ -188,14 +188,12 @@ func (t *SimpleChaincode) read (stub shim.ChaincodeStubInterface, args []string)
 	var key, jsonResp string
 	var err error
 
-	if len(args) != 1 {
+	/*if len(args) != 1 {
 		return nil, errors.New("Incorrect number of arguments. Expecting name of the key to query")
 
 	}
 
 	key = args[0]
-
-	return nil, errors.New("key: " + key)
 
 	valAsbytes, err := stub.GetState(key)
 
@@ -203,7 +201,7 @@ func (t *SimpleChaincode) read (stub shim.ChaincodeStubInterface, args []string)
         jsonResp = "{\"Error\":\"Failed to get state for " + key + "\"}"
         return nil, errors.New(jsonResp)
     }
-
-    return valAsbytes, nil
+*/
+    return nil, nil
 }
 
