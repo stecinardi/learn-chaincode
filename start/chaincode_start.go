e79139806fd1eac7b70ef6f4a697202a24e9f609
Stefano Cinardi <stefano.cinardi@gmail.com>
2016-10-07 11:19:42 +0200

    utilizzo marshaller per conversione struct in json 3

--- start/chaincode_start.go
+++ start/chaincode_start.go
@@ -50,7 +50,7 @@ func main() {
 }
 
 // Init resets all the things
-func (t *SimpleChaincode) Init(stub shim.ChaincodeStubInterface, function string, args []string) ([]byte, error) {
+func (t *SimpleChaincode) Init(stub *shim.ChaincodeStub, function string, args []string) ([]byte, error) {
 	if len(args) != 1 {
 		return nil, errors.New("Incorrect number of arguments. Expecting 1")
 	}
@@ -64,7 +64,7 @@ func (t *SimpleChaincode) Init(stub shim.ChaincodeStubInterface, function string
 }
 
 // Invoke is our entry point to invoke a chaincode function
-func (t *SimpleChaincode) Invoke(stub shim.ChaincodeStubInterface, function string, args []string) ([]byte, error) {
+func (t *SimpleChaincode) Invoke(stub *shim.ChaincodeStub, function string, args []string) ([]byte, error) {
 	fmt.Println("invoke is running " + function)
 
 	// Handle different functions
@@ -81,7 +81,7 @@ func (t *SimpleChaincode) Invoke(stub shim.ChaincodeStubInterface, function stri
 	return nil, errors.New("Received unknown function invocation")
 }
 
-func (t *SimpleChaincode) write (stub shim.ChaincodeStubInterface, args []string) ([]byte, error) {
+func (t *SimpleChaincode) write (stub *shim.ChaincodeStub, args []string) ([]byte, error) {
 	var key,value string
 	var err error
 	fmt.Println("running write()")
@@ -101,7 +101,7 @@ func (t *SimpleChaincode) write (stub shim.ChaincodeStubInterface, args []string
 	return nil, nil
 }
 
-func (t *SimpleChaincode) init_watch (stub shim.ChaincodeStubInterface, args []string) ([]byte, error) {
+func (t *SimpleChaincode) init_watch (stub *shim.ChaincodeStub, args []string) ([]byte, error) {
 	
 	var err error
 	
@@ -172,7 +172,7 @@ func (t *SimpleChaincode) init_watch (stub shim.ChaincodeStubInterface, args []s
 }
 
 // Query is our entry point for queries
-func (t *SimpleChaincode) Query(stub shim.ChaincodeStubInterface, function string, args []string) ([]byte, error) {
+func (t *SimpleChaincode) Query(stub *shim.ChaincodeStub, function string, args []string) ([]byte, error) {
 	fmt.Println("query is running " + function)
 
 	// Handle different functions
@@ -184,7 +184,7 @@ func (t *SimpleChaincode) Query(stub shim.ChaincodeStubInterface, function strin
 	return nil, errors.New("Received unknown function query")
 }
 
-func (t *SimpleChaincode) read (stub shim.ChaincodeStubInterface, args []string) ([]byte, error) {
+func (t *SimpleChaincode) read (stub *shim.ChaincodeStub, args []string) ([]byte, error) {
 	var key, jsonResp string
 	var err error
 
