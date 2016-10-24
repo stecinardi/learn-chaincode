099029f45ea50d64b3c4fa700649224535dc4b2b
Stefano Cinardi <stefano.cinardi@gmail.com>
2016-10-24 17:19:36 +0200

    aggiunti commenti

--- start/chaincode_start.go
+++ start/chaincode_start.go
@@ -376,6 +376,7 @@ func (t *SimpleChaincode) registerWatch (stub *shim.ChaincodeStub, args []string
 	jsonUserAsBytes, err := json.Marshal(user)
 	if err != nil {
 		fmt.Println("error: ", err)
+		fmt.Printf("! user index: %+v ", user)
 	}
 
 	err = stub.PutState(codCliente, jsonUserAsBytes)								//rewrite the watch with id as key
